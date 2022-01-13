e98c055e5f50d5372d57942405f41abc0a3f6bd0
Sidhaant Thakker <sidhaantthakker@gmail.com>
2022-01-14 03:39:52 +0530

    installed ejs dependencies and setup ejs

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const methodOverride =  require('method-override');
+
+
 const db = require('./triggerFunctions');
 
-const port = 8080;
+const PORT = 8080;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(methodOverride("_method"));
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get('/triggers', db.getAllTriggers);
 app.get('/triggers/:uuid', db.getOneTrigger);
@@ -15,5 +23,5 @@ app.put('/triggers/:uuid', db.putTrigger);
 app.delete('/triggers/:uuid', db.deleteTrigger);
 
 app.listen(port, () => {
-    console.log(`App running on port: ${port}`)
+    console.log(`App running on port: ${PORT}`);
 });
